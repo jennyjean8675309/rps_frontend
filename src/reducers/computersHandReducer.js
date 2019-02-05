28aadfbab67339ea20a1036c996a8951154a18c9
jennyjean <jennyjean8675309@yahoo.com>
2019-02-05 12:25:16 -0500

    Refactored computer algorithms for choosing cards

--- src/reducers/computersHandReducer.js
+++ src/reducers/computersHandReducer.js
@@ -1,49 +1,51 @@
 export const computersHandReducer = (computersHand = [], action) =>{
   switch (action.type) {
     case 'COMPUTER_SELECTS_SOLDIERS':
-      // console.log('computer choosing soldiers...', action.computersDeal)
-      let cards = []
-      action.computersDeal.forEach(card =>{
-        if (cards[card.soldier_type_id]){
-          cards[card.soldier_type_id]++
+      let deal = action.computersDeal;
+      let rocks = [];
+      let papers = [];
+      let scissors = [];
+      deal.forEach(card => {
+        if (card.soldier_type_id === 1) {
+          rocks.push(card)
+        } else if (card.soldier_type_id === 2) {
+          papers.push(card)
         } else {
-          cards[card.soldier_type_id] = 1
+          scissors.push(card)
         }
       })
-      // console.log(cards)
-      let cardArray = []
-      let card_type;
-      for (card_type in cards) {
-      cardArray.push({ card_type: cards[card_type]})
-      }
-      // console.log(cardArray)
-      // cards.sort(function (a, b){
-      //   return a.value - b.value;
-      // })
-      return [...action.computersDeal.slice(0, 5)]
+      let sorted = [rocks, papers, scissors].sort(function (a, b) {
+        return b.length - a.length
+      })
+      return [...sorted.flat().slice(0, 5)]
     case 'COMPUTER_SELECTS_UPGRADES':
-      console.log('computer choosing...', action.computersDeal, action.computersHand)
-      // let types = []
-      // action.computersHand.forEach(card => types.push(card.soldier_type_id))
-      // console.log(types)
-      // let newHand = [...computersHand]
-      // action.computersDeal.forEach(card =>{
-      //   while (newHand.length < 10 && newHand.includes(card) === false){
-      //     if (types.includes(card.soldier_type_id) && card.points === 3){
-      //       newHand.push(card)
-      //     }
-      //   }
-      //   while (newHand.length < 10 && newHand.includes(card) === false){
-      //     if (types.includes(card.soldier_type_id) && card.points === 2){
-      //       newHand.push(card)
-      //     }
-      //   }
-      //   while (newHand.length < 10 && newHand.includes(card) === false){
-      //     newHand.push(card)
-      //   }
-      // console.log(newHand)
-      // })
-      return [...computersHand, ...action.computersDeal.slice(0, 5)]
+      let types = []
+      action.computersHand.forEach(card => types.push(card.soldier_type_id))
+      console.log(types)
+      let newHand = [...computersHand]
+      action.computersDeal.map(card => {
+        if (types.includes(card.soldier_type_id)) {
+          newHand.push(card)
+        }
+        return newHand
+      })
+      let reducedDeal = action.computersDeal.filter(card => newHand.includes(card) === false)
+      console.log(reducedDeal)
+      reducedDeal.sort(function (a, b) {
+        return a.points - b.points
+      })
+      if (newHand.length === 10){
+        return newHand
+      } else if (newHand.length > 10) {
+        return newHand.slice(0, 10)
+      } else {
+        let i = 0
+        do {
+          newHand.push(reducedDeal[i])
+          i++
+        } while (newHand.length < 10);
+        return newHand
+      }
     case 'COMPUTER_DEPLOY_ARMY':
       console.log("computer making it's choice...", action.selectedArmy)
       let army = [...computersHand].filter(card => card.soldier_type_id === action.selectedArmy)
